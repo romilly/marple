93d604c1b47df36b4ebcffff7d3a5bb6cbc469e7
Romilly Cocking <romilly.cocking@gmail.com>
2026-04-26 14:50:14 +0100

    Cleaned test_preeicated_disjoint

--- tests/unit/array/test_predicates_disjoint.py
+++ tests/unit/array/test_predicates_disjoint.py
@@ -8,32 +8,34 @@ arithmetic to character codepoints if these predicates overlapped.
 from marple.numpy_aplarray import NumpyAPLArray
 from marple.backend_functions import str_to_char_array
 
+from marple.adapters.numpy_array_builder import BUILDER
+
 
 class TestPredicatesDisjoint:
     def test_char_vector_is_char_not_numeric(self) -> None:
-        a = NumpyAPLArray([3], str_to_char_array("abc"))
+        a = BUILDER.apl_array([3], str_to_char_array("abc"))
         assert a.is_char()
         assert not a.is_numeric()
 
     def test_empty_char_vector_is_char_not_numeric(self) -> None:
-        a = NumpyAPLArray([0], str_to_char_array(""))
+        a = BUILDER.apl_array([0], str_to_char_array(""))
         assert a.is_char()
         assert not a.is_numeric()
 
     def test_int_vector_is_numeric_not_char(self) -> None:
-        a = NumpyAPLArray.array([3], [1, 2, 3])
+        a = BUILDER.apl_array([3], [1, 2, 3])
         assert a.is_numeric()
         assert not a.is_char()
 
     def test_float_vector_is_numeric_not_char(self) -> None:
-        a = NumpyAPLArray.array([2], [1.0, 2.0])
+        a = BUILDER.apl_array([2], [1.0, 2.0])
         assert a.is_numeric()
         assert not a.is_char()
 
     def test_boolean_result_is_numeric_not_char(self) -> None:
         # Results of comparisons are boolean/numeric, never char.
-        a = NumpyAPLArray.array([3], [1, 2, 3])
-        b = NumpyAPLArray.array([3], [2, 2, 2])
+        a = BUILDER.apl_array([3], [1, 2, 3])
+        b = BUILDER.apl_array([3], [2, 2, 2])
         result = a.less_than(b)
         assert result.is_numeric()
         assert not result.is_char()
