11cdf671ff33d2270d0ffb696d778619b9deebc3
Romilly Cocking <romilly.cocking@gmail.com>
2026-04-28 10:19:41 +0100

    fixed import

--- tests/unit/workspace/test_workspace.py
+++ tests/unit/workspace/test_workspace.py
@@ -3,7 +3,7 @@
 import os
 import tempfile
 
-from marple.backend_functions import str_to_char_array
+from marple.ports.array import str_to_char_array
 from marple.engine import Interpreter
 from marple.workspace import save_workspace, load_workspace, list_workspaces
 from marple.adapters.numpy_array_builder import BUILDER
